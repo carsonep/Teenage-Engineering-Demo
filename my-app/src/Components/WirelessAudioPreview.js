b1906c55690ed84b97c4f564afef2c868a086448
carsonep <carsonepeterson@gmail.com>
2021-11-29 21:15:52 -0600

    Added Links to Audio Preview Page

--- my-app/src/Components/WirelessAudioPreview.js
+++ my-app/src/Components/WirelessAudioPreview.js
@@ -9,43 +9,65 @@ import orthoHero from "../images/orthoHero.png";
 import od11Hero from "../images/od11Hero.png";
 import op1Acc from "../images/op1Acc.jpg";
 import cameraHero from "../images/cameraHero.png";
+import { NavLink } from "react-router-dom";
 
 function WirelessAudioPreview() {
   return (
-    <div className="">
-      <NavBar bgColor={"#000000"} textColor={"#FFFFfF"} />
+    <div className="product__preview">
+      <NavBar textColor={"#FFFFfF"} />
       <div className="grid grid-cols-6">
-        <div
-          // className="col-span-6 h-full"
-          className="-mt-14 md:-mt-14 lg:-mt-44 w-full col-span-6"
-          style={{
-            backgroundImage: `url(${ob4Hero})`,
-            backgroundSize: "100%",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "50% 60%",
-            width: "100%",
-            height: 0,
-            paddingTop: "70%",
-          }}
-        ></div>
+        <NavLink
+          to="/products/ob-4"
+          className="md:-mt-14 lg:-mt-44 w-full col-span-6  mt-2 md:mt-0"
+        >
+          <div
+            className="rounded-2xl md:rounded-none"
+            style={{
+              backgroundImage: `url(${ob4Hero})`,
+              backgroundSize: "100%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "50% 60%",
+              width: "100%",
+              height: 0,
+              paddingTop: "70%",
+            }}
+          ></div>
+        </NavLink>
 
-        <div className="col-span-3" style={{ backgroundColor: "#e5e5e5" }}>
+        <NavLink
+          to="/products/orthoremote"
+          className="col-span-6 md:col-span-3 rounded-2xl md:rounded-none mt-2 md:mt-0"
+          style={{ backgroundColor: "#e5e5e5" }}
+        >
           <img src={orthoHero} />
-        </div>
-        <div className="col-span-3">
-          <img src={od11Hero} />
-        </div>
+        </NavLink>
 
-        <div
-          className="col-span-6 h-48 flex justify-center items-center"
+        <NavLink
+          to="/products/od-11"
+          className="col-span-6 md:col-span-3 mt-2 md:mt-0"
+          style={{ backgroundColor: "#e5e5e5" }}
+        >
+          <img className="rounded-2xl md:rounded-none" src={od11Hero} />
+        </NavLink>
+
+        <NavLink
+          to="/store"
+          className="col-span-6 h-48 flex justify-center items-center rounded-2xl md:rounded-none mt-2 md:mt-0"
           style={{
             backgroundColor: "#0f0e12",
           }}
         >
-          <h1 className="text-white text-2xl md:text-5xl">
-            worldwide delivery. visit our store.
-          </h1>
-        </div>
+          <div
+            className="rounded-2xl md:rounded-none"
+            style={{
+              backgroundColor: "#0f0e12",
+            }}
+          >
+            <h1 className="text-white text-2xl md:text-5xl">
+              worldwide delivery. visit our store.
+            </h1>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
